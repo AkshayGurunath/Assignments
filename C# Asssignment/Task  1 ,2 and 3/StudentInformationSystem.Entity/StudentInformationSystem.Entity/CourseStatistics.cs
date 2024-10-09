94569062b48ca3e9c79cab1940478243b426996c
AkshayParab <117382696+AkshayGurunath@users.noreply.github.com>
2024-10-09 12:30:03 +0530

    Task 1,2 and 3 completed

---
+++ C# Asssignment/Task  1 ,2 and 3/StudentInformationSystem.Entity/StudentInformationSystem.Entity/CourseStatistics.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentInformationSystem.Entity
+{
+    public class CourseStatistics
+    {
+        public int CourseID { get; set; }
+        public int NumberOfEnrollments { get; set; }
+        public decimal TotalPayments { get; set; }
+    }
+}
