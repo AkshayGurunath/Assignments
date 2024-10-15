be72774d455c203d7bf5fefd771d46df2a0a4d0c
AkshayParab <117382696+AkshayGurunath@users.noreply.github.com>
2024-10-15 17:42:11 +0530

    Add files via upload

---
+++ C# Asssignment/Task 11/StudentInformationSystem/StudentInformationSystem/dao/EnrollmentReportGenerator.cs
@@ -0,0 +1,58 @@
+﻿using StudentInformationSystem.util;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace StudentInformationSystem
+{
+    public class EnrollmentReportGenerator : IDisposable
+    {
+        private SqlConnection connection;
+
+        public EnrollmentReportGenerator()
+        {
+            // Use DBCUtil to get the SqlConnection
+            connection = DBCUtil.GetDBConnection();
+            connection.Open();
+        }
+
+        public List<string> GenerateEnrollmentReport(string courseName)
+        {
+            var enrollmentList = new List<string>();
+
+            // SQL query to retrieve students enrolled in the specified course
+            string query = @"
+                SELECT s.first_name, s.last_name 
+                FROM Students s
+                JOIN Enrollments e ON s.student_id = e.student_id
+                JOIN Courses c ON e.course_id = c.course_id
+                WHERE c.course_name = @courseName";
+
+            using (var command = new SqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@courseName", courseName);
+
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        string studentName = $"{reader["first_name"]} {reader["last_name"]}";
+                        enrollmentList.Add(studentName);
+                    }
+                }
+            }
+
+            return enrollmentList;
+        }
+
+        // Method to close and dispose the connection when the object is disposed
+        public void Dispose()
+        {
+            if (connection != null && connection.State == System.Data.ConnectionState.Open)
+            {
+                connection.Close();
+                connection.Dispose();
+            }
+        }
+    }
+}
---
+++ C# Asssignment/Task 11/StudentInformationSystem/StudentInformationSystem/util/DbConnection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentInformationSystem.util
+{
+    public class DBCUtil
+    {
+        // Method to get SqlConnection object using the provided property file
+        public static SqlConnection GetDBConnection()
+        {
+            // Get the connection string from the property file
+            string connectionString = "Data Source=LAPTOP-2H2P4CPV;Initial Catalog=SISDB;Integrated Security=True;Encrypt=False";
+            SqlConnection connection = new SqlConnection(connectionString); ;
+           
+            return connection;
+        }
+    }
+}
+
